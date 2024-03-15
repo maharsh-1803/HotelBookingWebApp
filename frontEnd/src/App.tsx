dd636a2c7f8e5b644d81df63174e17eee95493c7
maharsh-1803 <maharshpatel1803@gmail.com>
2024-03-15 08:03:59 +0530

    signIn

--- frontEnd/src/App.tsx
+++ frontEnd/src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router , Routes ,Route, Navigate} from 'react-router-dom';
 import Layouts from './layouts/Layouts';
 import Register from './pages/Register';
+import SignIn from './pages/SignIn';
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
           <Route path='/search' element={<Layouts><p>Search Page</p></Layouts>}></Route>
           <Route path='/register' element={<Layouts><Register/></Layouts>}></Route>
           <Route path='*' element={<Navigate to='/' />}></Route>
+          <Route path='/sign-in' element={<Layouts><SignIn/></Layouts>}/>
         </Routes>
       </Router>
     </div>
